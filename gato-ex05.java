98137bf99c883497864b4c31fdb025a2345d7cfa
mafê <134291043+lopesmafe9@users.noreply.github.com>
2024-09-06 09:09:10 -0300

    Create gato-ex05.java

---
+++ gato-ex05.java
@@ -0,0 +1,9 @@
+public class gato extends animal {
+    public void comer() {
+        System.out.println("gato comeu!");
+    }
+
+    public void miar() {
+        System.out.println("meow meow!");
+    }
+}
