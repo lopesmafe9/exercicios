0e5032141d22acd4987eeb3dd6ea5fd251c943b9
mafê <134291043+lopesmafe9@users.noreply.github.com>
2024-09-06 09:05:03 -0300

    Create produto-ex03.java

---
+++ produto-ex03.java
@@ -0,0 +1,12 @@
+public class produto {
+     public void exibirInformacoes(String nome) {
+        System.out.println("Nome: " + nome);
+    }
+    public void exibirInformacoes(String nome, int codigo) {
+        System.out.println("Nome: " + nome + ", Código: " + codigo);
+    }
+    public void exibirInformacoes(String nome, int codigo, double preco) {
+        System.out.println("Nome: " + nome + ", Código: " + codigo + ", Preço: R$ " + preco);
+    }
+}
+    
