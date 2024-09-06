d39e1cd1049d09aa661cce7ea82405dad1e832f2
mafê <134291043+lopesmafe9@users.noreply.github.com>
2024-09-06 09:07:22 -0300

    Create cachorro-ex05.java

---
+++ cachorro-ex05.java
@@ -0,0 +1,9 @@
+public class cachorro extends animal {
+    public void comer() {
+        System.out.println("cachorro comeu!");
+    }
+
+    public void latir() {
+        System.out.println("Au au!");
+    }
+}
